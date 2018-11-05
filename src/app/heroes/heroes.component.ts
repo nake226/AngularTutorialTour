06d5c75088586ba59fb7fe9efe50a3403b2b82b6
中井　貴之 <uu135923@fs276edb41.tkyc512.ap.nuro.jp>
2018-11-05 11:31:10 +0900

    add: ヒーローズコンポーネント

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.styl']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+  // a lifecyclehook called soon after components were generated.
+  ngOnInit() {
+  }
+
+  hero = 'Windstorm';
+
+}
