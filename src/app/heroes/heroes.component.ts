ee7eddd46cd0617af0994d378ab8ec9d2915ebb3
中井　貴之 <uu135923@fs276edb41.tkyc512.ap.nuro.jp>
2018-11-05 12:36:58 +0900

    add: イベント / ディレクティブ / クラスバインド / heroes配列 finish: 2. 2. Displaying a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,10 +8,14 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.styl']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // ヒーロー配列
+  heroes = HEROES;
+  // 選択したヒーロー
+  selectedHero: Hero;
+  // クリックしたヒーローを割り当てる
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
